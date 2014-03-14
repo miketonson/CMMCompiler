4e20c8c91c6546a0b87a8d812bdd7d2b2cfb757d
miketonson <miketonson1006@qq.com>
2014-03-14 17:22:54 +0800

    add some discription for the main method

--- compiler1/main.c
+++ compiler1/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  this is the main method for the complier,
+ *                for now it is used for lyntax, and there
+ *                may have a tree method under this.
+ * =====================================================================================
+ */
 int main(int argc, char** argv){
 	if(argc <= 1)
 		return 1;
